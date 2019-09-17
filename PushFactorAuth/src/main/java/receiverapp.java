c713b1d9c729b62ec2833289e6896ee80140a4f6
MacManDeluxe <btdorn+github@gmail.com>
2019-09-17 15:51:31 -0700

    dummy app functional
    Dummy receiver app with hardcoded input string parses, displays possible responses, accepts user input, then displays the corresponding response code. gitignore updated to exclude unnecessary build files.

--- PushFactorAuth/src/main/java/receiverapp.java
+++ PushFactorAuth/src/main/java/receiverapp.java
@@ -1,18 +1,32 @@
-//import whatever I need
+//import java.awt.*;
+//import javax.swing.*;
+import java.util.Scanner;
+
 //networking packages - listen on network for auth codes
 //first up: read text file to get "network string"
 //parse text of auth codes: separate "text" from random#
 class receiverapp
 {
-  public static void main(String args[])
+  public static void main(String[] args)
   {
     System.out.println("Receiver App!");
     //generate what will be the passed "network string"
-    String inputString = "1234approve1234deny";
+    String inputString = "1234approve-5678approve 10 min-4321deny";
     System.out.println(inputString);
     //parse string, separate into auth strings and option text strings
+    String[] codes = inputString.split("-");
     //print strings
+    System.out.println("Login request from a real site!");
+    System.out.println("Select One:");
+    for(int i=0; i<codes.length; i++)
+    {
+      String codeOut = codes[i].substring(4); //cuts out response code
+      System.out.println((i + 1) + ". " + codeOut);
+    }
     //request input for which option to select
-    //based on input, cat auth string onto response string and save to file
+    Scanner responseReq = new Scanner(System.in);
+    int response = responseReq.nextInt();
+    //based on input, output response code
+    System.out.println(codes[response-1].substring(0,4));
   }
 }
