1bf6c4fd1093bcd8337e696d96782aa9d564be6d
MacManDeluxe <btdorn+github@gmail.com>
2019-09-16 18:31:32 -0700

    receiverapp_outline
    moved receiverapp.java to src folder, typed up a quick outline for the first stage of the build

---
+++ src/receiverapp.java
@@ -0,0 +1,16 @@
+//import whatever I need
+//networking packages - listen on network for auth codes
+//first up: read text file to get "network string"
+//parse text of auth codes: separate "text" from random#
+class receiverapp
+{
+  public static void main(String args[])
+  {
+    System.out.println("Receiver App!");
+    //read text file to get "network string"
+    //parse string, separate into auth strings and option text strings
+    //print strings
+    //request input for which option to select
+    //based on input, cat auth string onto response string and save to file
+  }
+}
