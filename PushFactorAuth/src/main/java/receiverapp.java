bb686bbb1db9cb7836bbce7be16f251b8c41f8f8
MacManDeluxe <btdorn+github@gmail.com>
2019-09-19 11:11:17 -0700

    built and removed commented "old way"

--- PushFactorAuth/src/main/java/receiverapp.java
+++ PushFactorAuth/src/main/java/receiverapp.java
@@ -69,29 +69,6 @@ public class receiverapp
         try { socket.close(); } catch (IOException e) {}
         System.out.println("Closed: " + socket);
       }
-
-
-
-
-      /* old way
-      //generate what will be the passed "network string"
-      //String inputString = "1234approve-5678approve 10 min-4321deny";
-      System.out.println("Auth request received: " + inputString);
-      //parse string, separate into auth strings and option text strings
-      String[] codes = inputString.split("-");
-      //print strings
-      System.out.println("Login request from a real site!");
-      System.out.println("Select One:");
-      for (int i = 0; i < codes.length; i++) {
-        String codeOut = codes[i].substring(4); //cuts out response code
-        System.out.println((i + 1) + ". " + codeOut);
-      }
-      //request input for which option to select
-      Scanner responseReq = new Scanner(System.in);
-      int response = responseReq.nextInt();
-      //based on input, output response code
-      System.out.println(codes[response - 1].substring(0, 4));
-      */
     }
   }
 }
